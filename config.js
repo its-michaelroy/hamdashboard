3f653e4c064e1ba719e2b9e660352a481e188dfe
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-12-22 15:22:34 -0500

    Created updates and added menu options

--- config.js
+++ config.js
@@ -13,13 +13,19 @@ window.aURL = [
   ["add10d", "BACK", "#", "1", "R"],
   ["ff9100", "Refresh", "#", "1"],
   ["0dd1a7", "Help", "#", "1"],
-  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/VA3HDL", "1.7"],
+  ["2196F3", "CLUBLOG", "https://clublog.org/livestream/PY3TX", "1.7"],
   [
     "2196F3",
     "CONTEST",
-    "https://www.contestcalendar.com/fivewkcal.html",
+    "https://www.contestcalendar.com/weeklycont.php",
     "1",
-  ],
+    ],
+    [
+        "2196F3",
+        "CONTEST RUN",
+        "https://contest.run",
+        "1",
+    ],
   ["2196F3", "DX CLUSTER", "https://dxcluster.ha8tks.hu/map/", "1"],
   [
     "2196F3",
@@ -27,15 +33,29 @@ window.aURL = [
     "https://map.blitzortung.org/#3.87/-15/-55",
     "1",
     "R",
-  ],
-  ["2196F3", "PISTAR", "http://pi-star.local/", "1.2"],
+    ],
+    [
+        "2196F3",
+        "DX HEAT",
+        "https://www.dxheat.com/dxc/",
+        "1"
+    ],
+    ["2196F3", "PISTAR", "http://pi-star.local/", "1.2"],
+    ["2196F3", "APRS MAP", "https://aprs.fi/#!lat=-22.83050&lng=-43.21920", "1.2"],
   [
     "2196F3",
     "RADAR",
     "https://www.redemet.aer.mil.br/?i=produtos&p=radar-meteorologico",
     "1",
     "R"
-  ],
+    ],
+    [
+        "2196F3",
+        "Digital Mode Spotting",
+        "https://pskreporter.info/pskmap.html",
+        "1",
+        "R"
+    ],
   ["2196F3", "TIME.IS", "https://time.is/", "1", "R"],
   [
     "2196F3",
@@ -60,9 +80,9 @@ window.aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 window.aIMG = [
-  ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
+    ["RADAR", "https://s0.cptec.inpe.br/grafico/Satelite/goes16/realcadas/pagina/g16ch13_cptec.jpg"],
   [
-    "LOCAL RADAR",
+    "#LOCAL RADAR",
     "https://radar.weather.gov/ridge/standard/KNQA_loop.gif",
   ],
   [
@@ -74,31 +94,33 @@ window.aIMG = [
     "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
   ],
   [
-    "SATELLITE CAN",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/can/EXTENT3/GOES16-CAN-EXTENT3-1125x560.gif",
+    "SAT CGL",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/GEOCOLOR/GOES16-SSA-GEOCOLOR-900x540.gif",
   ],
   [
-    "SATELLITE BRAZIL",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/cgl/EXTENT3/GOES16-CGL-EXTENT3-600x600.gif",
+    "SAT LAST Hour",
+    "https://ciram.epagri.sc.gov.br/ciram_arquivos/meteorologia/satelite/ultima1zoom.png",
   ],
   [
-    "LIGHTNING",
-    "iframe|https://www.lightningmaps.org/blitzortung/america/index.php?bo_page=archive&lang=en&bo_map=south_america&bo_animation=now"],
+    "THUNDERSTORMS",
+      "iframe|https://embed.windy.com/embed2.html?lat=-15.8900&lon=-47.9309&detailLat=-15.8900&detailLon=-47.9309&width=900&height=600&zoom=11&level=surface&overlay=thunder&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=true&metricWind=km/h&metricTemp=%C2%B0C&radarRange=-1",
+],
   [
-    "LIGHTNING LOCAL",
-    "https://www.blitzortung.org/en/Images/image_b_sa.png",
+    "LIGHTNING & WEATHER",
+    "iframe|https://www.climaeradar.com.br/radarclima?center=-16.32,-47.92&zoom=8.33&layer=lr",
   ],
-  ["YOUTUBE EXAMPLE", "iframe|https://www.youtube.com/embed/fzPFaXAV_2Y?autoplay=1&mute=1"],
+    ["YOUTUBE CB", "iframe|https://www.youtube.com/embed/rxqjedCYUms?si=3fTQZg9-l70XTebi?autoplay=1&mute=1"],
   [
-    "WEBSITE EXAMPLE",
+    "AIRPORT MAP",
     "iframe|https://globe.adsbexchange.com/?airport=SBUL",
   ],
-  ["VIDEO EXAMPLE", "https://himawari8.nict.go.jp/movie/720/20240611_pifd.mp4"],
+    ["RADIO BRASIL", "iframe|https://radio-brasil.com/band-news-fm-905-brasilia-1162"],
   ["HF PROPAGATION",
     "https://www.hamqsl.com/solar101vhf.php"],
   [
-    "RS-44 POSITION",
-    "https://www.heavens-above.com/orbitdisplay.aspx?icon=default&width=600&height=300&mode=M&satid=44909",
+    "ISS & SO-50 POSITION",
+    "https://www.heavens-above.com/orbitdisplay.aspx?icon=iss&width=600&height=300&mode=M&satid=25544",
+    "https://www.heavens-above.com/orbitdisplay.aspx?icon=default&width=600&height=300&mode=M&satid=27607",
   ],
   ["Solar WX",
     "https://services.swpc.noaa.gov/images/animations/suvi/primary/map/latest.png"],
